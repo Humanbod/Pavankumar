a22af863f9bb15e906801994f575fd51e1ad5dfa
Humanbod <154618974+Humanbod@users.noreply.github.com>
2025-01-31 13:06:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ document.getElementById('download-text').addEventListener('click', function () {
   const blob = new Blob([content], { type: 'text/plain' });
   const link = document.createElement('a');
   link.href = "https://drive.google.com/file/d/1VNa2UCzE_Vt0ujuUYzb0Hi4s_RW9pnu0/view?usp=drivesdk".createObjectURL(blob);
-  link.download = 'example.txt';
+  link.download = 'https://drive.google.com/file/d/1VNa2UCzE_Vt0ujuUYzb0Hi4s_RW9pnu0/view?usp=drivesdk';
   link.click();
   URL.revokeObjectURL(link.href);
 });
